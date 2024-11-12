8b139db1016074657833360bc424252dee9f1ad4
Faraz Rostami <faraz.rostami@gmail.com>
2024-11-12 10:33:35 +0100

    Adds showmenu method in a while loop, a switch case

--- multifabriken/src/App.java
+++ multifabriken/src/App.java
@@ -6,8 +6,38 @@ public class App {
     private static ArrayList<Candy> candies = new ArrayList<>();
     private static ArrayList<Pipe> pipes = new ArrayList<>();
     private static ArrayList<OatMilk> oatMilks = new ArrayList<>();
-    public static void main(String[] args) {
-        Scanner Scanner = new Scanner(System.in);
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hej, välkommen till Multifabriken!");
+
+        while (true) {
+            showMenu();
+            int alternativ = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (alternativ) {
+                case 1:
+                    System.out.println("Du har valt att beställa en bil");
+                    break;
+                case 6:
+                    System.out.println("Programmet avslutas.");
+                    scanner.close();
+                    return;
+                default:
+                    System.out.println("Ogiltigt val.");
+                    break;
+            }
+        }
+    }
+
+    private static void showMenu() {
+        System.out.println("Välj ett alternativ:");
+        System.out.println("1. Beställa bil");
+        System.out.println("2. Beställa godis");
+        System.out.println("3. Beställa rör");
+        System.out.println("4. Beställa havremjölk");
+        System.out.println("5. Lista dina beställningar");
+        System.out.println("6. Avsluta programmet");
     }
 }
